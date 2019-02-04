0f6fe47d51b66f9bcf886ed0fd190c8bb56be1ff
Scott Wells <efwells101@gmail.com>
2019-02-04 09:28:27 -0700

    filter() string, only include non "f" words

--- snap-challenge.js
+++ snap-challenge.js
@@ -1,3 +1,15 @@
+//using javascript convert the string into an array and filter out any words that begin with the letter 'f'.
+let string = "Fate protects fools, little children and ships named Enterprise. You're going to be an interesting companion, Mr. Data. Some days you get the bear, and some days the bear gets you. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody. Why don't we just give everybody a promotion and call it a night - 'Commander'? Well, that's certainly good to know. Mr. Worf, you do remember how to fire phasers? A lot of things can change in twelve years, Admiral.";
+
+const array = string.toLowerCase().split(" ");
+
+const newArray = array.filter(index => !index.startsWith("f"));
+
+console.log(newArray);
+
+
+
+/*
 //using javascript and the filter array method remove all post from userId:5 from the array
 
 const array = [{"userId":1,"id":1,"title":"sunt aut facere repellat provident occaecati excepturi optio reprehenderit","body":"quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"},{"userId":1,"id":2,"title":"qui est esse","body":"est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"},{"userId":3,"id":30,"title":"a quo magni similique perferendis","body":"alias dolor cumque\nimpedit blanditiis non eveniet odio maxime\nblanditiis amet eius quis tempora quia autem rem\na provident perspiciatis quia"},{"userId":4,"id":31,"title":"ullam ut quidem id aut vel consequuntur","body":"debitis eius sed quibusdam non quis consectetur vitae\nimpedit ut qui consequatur sed aut in\nquidem sit nostrum et maiores adipisci atque\nquaerat voluptatem adipisci repudiandae"},{"userId":5,"id":45,"title":"ut numquam possimus omnis eius suscipit laudantium iure","body":"est natus reiciendis nihil possimus aut provident\nex et dolor\nrepellat pariatur est\nnobis rerum repellendus dolorem autem"},{"userId":5,"id":46,"title":"aut quo modi neque nostrum ducimus","body":"voluptatem quisquam iste\nvoluptatibus natus officiis facilis dolorem\nquis quas ipsam\nvel et voluptatum in aliquid"}];
@@ -8,7 +20,6 @@ console.table(newArray);
 
 
 
-/*
 //using javascript map array prototype and concatenation combine the post body with the post title.
 // to access the object, we may need to go two levels deep -- console.log(array[1.title]); -- object in an array
 // backticks keep track of ALL whitespace in between, including spaces and returns
@@ -19,6 +30,7 @@ const newArray = array.map(index => {
 });
 
 console.log(newArray);
+
 
 
 // delete only the "address" object from this array using map()
@@ -41,6 +53,7 @@ newArray = array.map(index => {
 });
 
 console.log(newArray);
+
 
 
 // delete only the "company" object from this array using forEach()
