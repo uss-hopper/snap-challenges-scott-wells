f637cd8927f6d6ea0b9cc98357f857bb04ed74e9
Scott Wells <efwells101@gmail.com>
2019-02-21 08:34:15 -0700

    reduce() a string, reverse the order

--- snap-challenge.js
+++ snap-challenge.js
@@ -1,11 +1,27 @@
+//using reduce reverse this string.
 
+let string = "I'll alert the crew. We finished our first sensor sweep of the neutral zone. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody.";
+
+let array = string.split(" ");
+
+let reducer = (acc, val) => acc + val;
+
+let newArray = array.reverse().reduce(reducer);
+//let newArray = array.reverse().join(" ");
+
+console.log(newArray);
+
+
+
+/*
+//use reduce to multiply this array
 const array = [1,2,3,5,8,13,21,34];
 
 const total = (acc, val) => acc * val;
 
 console.log(array.reduce(total));
 
-/*
+
 //turn the array below into a bootstrap card deck using javascript map.
 const posts =  [{"userId":1,"id":1,"title":"sunt aut facere repellat provident occaecati excepturi optio reprehenderit","body":"quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"},{"userId":1,"id":2,"title":"qui est esse","body":"est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"},{"userId":1,"id":3,"title":"ea molestias quasi exercitationem repellat qui ipsa sit aut","body":"et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"},{"userId":1,"id":4,"title":"eum et est occaecati","body":"ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"},{"userId":1,"id":5,"title":"nesciunt quas odio","body":"repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"}];
 
